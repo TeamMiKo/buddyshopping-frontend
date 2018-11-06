ab1c91ac16cd2b5ca274236fd4c205ca2c0633e7
Михаил Синяков <pvpshoot@gmail.com>
2018-11-06 17:51:49 +0400

    remove chunks

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -159,13 +159,7 @@ module.exports = {
         }
       })
     ],
-    // Automatically split vendor and commons
-    // https://twitter.com/wSokra/status/969633336732905474
-    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    splitChunks: {
-      chunks: "async",
-      name: false
-    },
+
     // Keep the runtime chunk seperated to enable long term caching
     // https://twitter.com/wSokra/status/969679223278505985
     runtimeChunk: true
